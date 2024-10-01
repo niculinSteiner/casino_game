4a1f053042e68b76e5f55f14ebff3cc8693ad7f5
Niculin Steiner <niculin.steiner@ergon.ch>
2024-10-01 10:15:07 +0200

    init

---
+++ game-server/src/test/java/ch/nicustei/gameserver/GameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.nicustei.gameserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
