4abda50dafebd7782e192f337b074a04215be5aa
niculinSteiner <niculin.steiner@edu.tbz.ch>
2024-10-29 11:35:15 +0100

    cross origin fix

--- game-server/src/main/java/ch/nicustei/gameserver/config/WebConfig.java
+++ game-server/src/main/java/ch/nicustei/gameserver/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*") // Allow all endpoints
+        registry.addMapping("/**") // Allow all endpoints
                 .allowedOrigins("http://localhost:3000") // Allow requests from the frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed methods
                 .allowedHeaders("") // Allow any headers
