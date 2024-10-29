1bdb43c884a8d051fa991a2c0eaf84c79020b58a
niculinSteiner <niculin.steiner@edu.tbz.ch>
2024-10-29 11:20:51 +0100

    cross origin fix

---
+++ game-server/src/main/java/ch/nicustei/gameserver/config/WebConfig.java
@@ -0,0 +1,21 @@
+package ch.nicustei.gameserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/*") // Allow all endpoints
+                .allowedOrigins("http://localhost:3000") // Allow requests from the frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed methods
+                .allowedHeaders("") // Allow any headers
+                .allowCredentials(true) // Allow credentials if needed
+                .maxAge(3600); // Cache the CORS configuration for an hour
+    }
+}
