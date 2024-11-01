dd079cd7a76dc2a49e8de8a8eae10cd70b74fe9b
nicustei <niculin.steiner@ergon.ch>
2024-11-01 14:00:31 +0100

    add some more java doc

--- game-server/src/main/java/ch/nicustei/gameserver/domain/service/GameService.java
+++ game-server/src/main/java/ch/nicustei/gameserver/domain/service/GameService.java
@@ -3,7 +3,18 @@ package ch.nicustei.gameserver.domain.service;
 import ch.nicustei.gameserver.domain.GameBet;
 import ch.nicustei.gameserver.domain.GameResult;
 
+/**
+ * Interface to manage the game mechanism. Contains core functions of each game.
+ *
+ * @param <T> Bet type which should be used for each implementation.
+ */
 public interface GameService<T extends GameBet> {
 
+    /**
+     * Method to start the game.
+     *
+     * @param bet bet, which should be validated against the game result.
+     * @return a game result, containing all required informations.
+     */
     GameResult startGame(T bet);
 }
